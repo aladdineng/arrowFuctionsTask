88d37db54daa3be96537682f4c062e560bcf63fb
aladdineng <aladdin.enbeh@gmail.com>
2024-05-01 19:32:14 +0300

    Day8 arrow01

--- index.js
+++ index.js
@@ -5,30 +5,29 @@
 	}
 ********************************/
 const greet = (name) => {
-    return `Hello, ${name}!`;
-}
-console.log(greet ("Aladdin"))
+  return `Hello, ${name}!`;
+};
+console.log(greet("Aladdin"));
 
 //********************************************************************************************** */
 // Q2) Write a simple arrow function that takes two parameters and returns their sum.
-let sum = (num1,num2) => {
-    return ( num1 + num2)
-}
-console.log(sum (15,5))
+let sum = (num1, num2) => {
+  return num1 + num2;
+};
+console.log(sum(15, 5));
 
 /********************************************************************************************** */
 // Q3) Write a simple arrow function that squares a number.
-let squrnum = (num1,num2) => {
-    return (num1 ** num2 )
-}
-console.log(squrnum(2,3))
+let squrnum = (num1, num2) => {
+  return num1 ** num2;
+};
+console.log(squrnum(2, 3));
 
 /************************************************************************************************ */
 // Q4) [🌶️🌶️] Create an arrow function that takes an array of numbers and returns a new array with each number squared.
 
-let numbers= [1,2,3,4,5] 
-let result = numbers.map((num)=>{
-return num**2
-})
-console.log(result)
-
+let numbers = [1, 2, 3, 4, 5];
+let result = numbers.map((num) => {
+  return num ** 2;
+});
+console.log(result);
